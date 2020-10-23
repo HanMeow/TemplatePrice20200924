076a8eceb2025d400663e76fa61d966493224c72
ml-hance <hance.jh@medialand.tw>
2020-10-23 17:35:55 +0800

    updated

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 
 ;(() => {
+  const page = document.querySelector("body > .login-page");
   const price = document.querySelector("body > div > div > .login-form > input.price");
   const result = document.querySelector("body > div > div > .login-form > input.result");
   price.oninput = () => {
     const { value } = price
     result.value = +value === 0 ? null : ((+value*0.95 + 0.5)|0)
+  }
+  if (page) {
+    page.style.removeProperty('display')
   }
 })();
